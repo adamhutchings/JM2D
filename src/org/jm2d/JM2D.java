d06cc3df28cedaf754cd4adb7bd718cd6a0491d3
adamhutchings <adam.abahot@gmail.com>
2021-05-19 12:19:37 -0700

    Add template for main code

---
+++ src/org/jm2d/JM2D.java
@@ -0,0 +1,22 @@
+package org.jm2d;
+
+/**
+ * Main class of the game. This runs everything, and its methods are called
+ */
+public final class JM2D {
+
+    private JM2D() {}
+
+    public static void init() {
+
+    }
+
+    public static void mainloop() {
+
+    }
+
+    public static void exit() {
+
+    }
+
+}
