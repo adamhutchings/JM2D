df0816b78858fee38434c7051726d0356a4bbd01
adamhutchings <adam.abahot@gmail.com>
2021-05-19 10:56:14 -0700

    Create basic main method

---
+++ src/org/jm2d/Main.java
@@ -0,0 +1,15 @@
+package org.jm2d;
+
+/**
+ * Contains the main method for the game.
+ */
+public class Main {
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
