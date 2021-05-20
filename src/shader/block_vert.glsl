6abdbc41ad96c5b4291d50a8ad027584255f6e0f
adamhutchings <adam.abahot@gmail.com>
2021-05-20 15:28:08 -0700

    Take in a camera uniform

--- src/shader/block_vert.glsl
+++ src/shader/block_vert.glsl
@@ -3,10 +3,14 @@
 layout (location=0) in vec3 position;
 layout (location=1) in vec2 texCoord;
 
+uniform vec2 camera_pos;
+
 out vec2 outTexCoord;
 
 void main()
 {
     gl_Position = vec4(position, 1.0);
+    gl_Position.x -= camera_pos.x;
+    gl_Position.y -= camera_pos.y;
     outTexCoord = texCoord;
 }
