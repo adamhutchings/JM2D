c9ad528be205bd67614682990a7552043248a404
adamhutchings <adam.abahot@gmail.com>
2021-05-20 15:27:26 -0700

    Add view for the camera position

---
+++ src/org/jm2d/gl/View.java
@@ -0,0 +1,23 @@
+package org.jm2d.gl;
+
+import static org.lwjgl.opengl.GL33.*;
+
+/**
+ * Describes the camera position.
+ */
+public final class View {
+
+    private View() {}
+
+    private static float x = 0.1f, y = 0.1f;
+
+    public static void setPosition(float newX, float newY) {
+        x = newX;
+        y = newY;
+    }
+
+    public static float[] getPos() {
+        return new float[]{x, y};
+    }
+
+}
